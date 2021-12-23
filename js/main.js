d8b1a53d52b2e0b8616df13740efab1819517d72
Sashane RV <detectivesherlockholmes4u@gmail.com>
2021-12-23 18:04:29 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let divContainer = document.querySelector("#container");
+
+function createSketchPad(n, m) {
+  let strHTML = "";
+  for (let i = 0; i < n * m; i++) {
+    strHTML += "<div class='pixel'></div>";
+  }
+  divContainer.innerHTML = strHTML;
+}
+
+createSketchPad(16, 16);
+
+let divPixel = document.querySelectorAll(".pixel");
+console.log(divPixel);
+
+function colorHover(e) {
+  e.target.style.background = "Red";
+}
+
+divPixel.forEach((data) => data.addEventListener("mouseover", colorHover));
