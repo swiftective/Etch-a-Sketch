eb13475c7a6bfdaa1d66bd748d572d5978594f9f
Sashane RV <detectivesherlockholmes4u@gmail.com>
2021-12-23 21:15:13 +0530

    Add range-slider and color-input and some refactor

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 let divContainer = document.querySelector("#container");
 
-function createSketchPad(n, m) {
-  let strHTML = "";
-  for (let i = 0; i < n * m; i++) {
-    strHTML += "<div class='pixel'></div>";
+function createSketchPad(n) {
+  for (let i = 0; i < n ** 2; i++) {
+    childNode = document.createElement("div");
+    childNode.classList = "pixel";
+    divContainer.appendChild(childNode);
   }
-  divContainer.innerHTML = strHTML;
 }
 
-createSketchPad(16, 16);
+createSketchPad(16);
 
 let divPixel = document.querySelectorAll(".pixel");
+
+divPixel.forEach((data) => data.addEventListener("mouseover", colorHover));
 
 function colorHover(e) {
   e.target.style.background = "Red";
 }
-
-divPixel.forEach((data) => data.addEventListener("mouseover", colorHover));
